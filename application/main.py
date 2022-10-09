9cb5bb39cd12cc63b3f8de13e8be06518852bac3
Florian Katerndahl <florian@katerndahl.com>
2022-10-09 12:22:22 +0200

    add help strings

--- application/main.py
+++ application/main.py
@@ -4,15 +4,20 @@ from protokoll2nextcloud.mail import MetaMail
 from protokoll2nextcloud.messages import Messages
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--imap", required=True, dest="imap", type=str)
-parser.add_argument("--user", required=True, dest="user", type=str)
-parser.add_argument("--password", required=True, dest="imap_passwd", type=str)
-parser.add_argument("--sender", required=True, dest="sender_address", type=str)
-parser.add_argument("--subject", required=True, dest="subject", type=str)
-parser.add_argument("--nc-password", required=True, dest="nextcloud_app_passwd", type=str)
-parser.add_argument("--nc-url", required=True, dest="nextcloud_webdav_url", type=str)
-parser.add_argument("--nc-dest", required=True, dest="nextcloud_destination_folder", type=str)
-parser.add_argument("--max-age", required=False, dest="max_age", type=int, default=0)
+parser.add_argument("--imap", required=True, dest="imap", type=str, help="Hostname of the IMAP server.")
+parser.add_argument("--user", required=True, dest="user", type=str, help="Username to use when logging in to the IMAP server and Nextcloud instance.")
+parser.add_argument("--password", required=True, dest="imap_passwd", type=str, help="Password to use when logging in to the IMAP server.")
+parser.add_argument("--sender", required=True, dest="sender_address", type=str, help="E-Mail address to search for when querying mails.")
+parser.add_argument("--subject", required=True, dest="subject", type=str, help="Mail subject to search for, currently allows for five typos.")
+parser.add_argument("--nc-password", required=True, dest="nextcloud_app_passwd", type=str, help="App password to use when logging into your Nextcloud "
+                                                                                                "instance.")
+parser.add_argument("--nc-url", required=True, dest="nextcloud_webdav_url", type=str, help="Nextcloud Web-DAV url used for file upload. Must include trailing "
+                                                                                           "slash.")
+parser.add_argument("--nc-dest", required=True, dest="nextcloud_destination_folder", type=str, help="Path inside your Nextcloud instance. Must include "
+                                                                                                    "trailing slash.")
+parser.add_argument("--max-age", required=False, dest="max_age", type=int, default=0, help="Oldest delivery date which is still processed. Don't specify or "
+                                                                                           "set to zero to process all messages which match the above given "
+                                                                                           "options.")
 
 args = ArgumentClass()
 parser.parse_args(namespace=args)
