ad6660ebe32eac3bcd127c7ef2725a9a4006772e
Florian Katerndahl <florian@katerndahl.com>
2022-10-09 14:53:27 +0200

    utilize temporary file and latex format for pdf conversion

--- protokoll2nextcloud/messages.py
+++ protokoll2nextcloud/messages.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Dict, Union
 from datetime import datetime
+import tempfile
+import os
 import pandoc
 import requests
 
@@ -14,13 +16,15 @@ class Messages:
         pass
 
     def normalize_and_upload(self, user: str, password: str, nc_webdav_url: str, nc_destination_path: str):
+        f: tempfile = tempfile.NamedTemporaryFile(suffix=".pdf", delete=False)
+
         for _, message_value in self.messages.items():
             delivery_date: datetime = datetime.strptime(message_value.get("date"), "%a, %d %b %Y %H:%M:%S %z")
             if message_value.get("subtype") != "pdf":
                 trick_pandoc_extensions = message_value.get("filename").split(".")[-1]
                 raw_attachment = message_value.get("file-content")
                 converted_attachment = pandoc.read(raw_attachment, format=trick_pandoc_extensions if trick_pandoc_extensions != "txt" else "t2t")
-                file_to_upload = pandoc.write(converted_attachment, format="pdf", options=["--pdf-engine=lualatex"])
+                file_to_upload = pandoc.write(converted_attachment, file=f.name, format="latex", options=["--pdf-engine=lualatex"])
             else:
                 file_to_upload = message_value.get("file-content")
 
@@ -28,3 +32,5 @@ class Messages:
                 s.auth = (user, password)
                 s.put(nc_webdav_url + nc_destination_path + "Protokoll vom " + datetime.strftime(delivery_date, "%d.%m.%Y") + ".pdf",
                       files={"file": file_to_upload})
+
+        os.unlink(f.name)
